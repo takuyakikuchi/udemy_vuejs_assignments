1b1dce129d2177bf17ab0d2436ead389df5dee2e
Takuya Kikuchi <kikuchi.takuya.dbce8@gmail.com>
2020-03-07 15:27:32 +0900

    completed exercise 7

---
+++ Exercise 7/src/components/Server/ServerDetails.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="col-xs-12 col-sm-6">
+    <p v-if="!server">Please select a server</p>
+    <p v-else>Selected server is #{{ server.id }}, status: {{ server.status }}</p>
+  </div>
+</template>
+
+<script>
+  import { serverBus } from '../../main.js'
+
+  export default {
+    data: function() {
+      return {
+        server: null
+      }
+    },
+    created() {
+      serverBus.$on('serverSelected', (server) => {
+        this.server = server;
+      });
+    }
+  }
+</script>
+
+<style>
+</style>
---
+++ Exercise 7/src/components/Server/Servers.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="col-xs-12 col-sm-6">
+    <ul class="list-group">
+      <Server 
+        v-for="server in servers"
+        :key="server.id"
+        :server="server"
+      />
+    </ul>
+  </div>
+  
+</template>
+
+<script>
+  import Server from './Server.vue';
+
+  export default {
+    data: function() {
+      return {
+        servers: [
+          { id: 1, status: 'Normal'},
+          { id: 2, status: 'Critical'},
+          { id: 3, status: 'Unknown'},
+          { id: 4, status: 'Normal'},
+        ]
+      }
+    },
+    components: {
+      Server,
+    }
+  }
+</script>
+
+<style>
+
+</style>

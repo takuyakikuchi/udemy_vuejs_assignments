e9adbccb89bc619a9703f85ee226e65672eb71f9
Takuya Kikuchi <kikuchi.takuya.dbce8@gmail.com>
2020-03-09 13:57:21 +0900

    exercise 9-1 done

---
+++ Exercise 9/src/App.vue
@@ -0,0 +1,94 @@
+<template>
+  <div class="container">
+    <form>
+      <div class="row">
+        <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
+          <div class="form-group">
+            <label for="name">Name</label>
+            <input
+              type="text"
+              class="form-control"
+              id="name"
+              v-model="userInfo.name"
+            />
+          </div>
+          <div class="form-group">
+            <label for="email">Email address</label>
+            <input
+              type="email"
+              class="form-control"
+              id="email"
+              aria-describedby="emailHelp"
+              v-model="userInfo.email"
+            />
+            <small
+              id="emailHelp"
+              class="form-text text-muted"
+            >We'll never share your email with anyone else.</small>
+          </div>
+          <div class="form-group">
+            <label for="password">Password</label>
+            <input
+              type="password"
+              class="form-control"
+              id="password"
+              v-model="userInfo.password"
+            />
+          </div>
+          <div class="form-group form-check">
+            <input
+              type="checkbox"
+              class="form-check-input"
+              id="save-check"
+              v-model="isStored"
+            />
+            <label class="form-check-label" for="save-check">Store data?</label>
+          </div>
+          <button type="submit" class="btn btn-primary">Submit</button>
+
+          <!-- Exercise 2 -->
+          <!-- Only display the Form if it has NOT been submitted -->
+          <!-- Display the Data Summary ONCE the Form HAS been submitted -->
+
+          <!-- Exercise 3 -->
+          <!-- Edit the Example from above and create a custom "Full Name" Control -->
+          <!-- which still holds the First Name and Last Name Input Field -->
+        </div>
+      </div>
+    </form>
+    <hr />
+    <div class="row">
+      <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
+        <div class="panel panel-default">
+          <div class="panel-heading">
+            <h4>Your Data</h4>
+          </div>
+          <div class="panel-body">
+            <p>Full Name: {{userInfo.name}}</p>
+            <p>Mail: {{userInfo.email}}</p>
+            <p>Password: {{userInfo.password}}</p>
+            <p>Store in Database?: {{isStored}}</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      userInfo: {
+        name: '',
+        email: '',
+        password: ''
+      },
+      isStored: false
+    }
+  }
+};
+</script>
+
+<style>
+</style>

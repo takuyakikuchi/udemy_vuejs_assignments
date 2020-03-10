38e8c2ec5343fd598b07e8d7e4d3fb8e597c6ab6
Takuya Kikuchi <kikuchi.takuya.dbce8@gmail.com>
2020-03-10 19:54:20 +0900

    11-2 done

--- Exercise 11/src/App.vue
+++ Exercise 11/src/App.vue
@@ -4,12 +4,7 @@
       <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
         <h1>Filters & Mixins</h1>
         <input type="text" v-model="userInput"/>
-        <div class="reverse-input">{{userInput | reverse}}</div>
-
-        <!-- Exercise 2 -->
-        <!-- Build a global Filter which counts the length of a word and it appends it -->
-        <!-- Like this: "Test" => Gets Filtered to => "Test (4)" -->
-
+        <div class="reverse-input">{{userInput | reverse}} ({{userInput | countLength}})</div>
         <!-- Exercise 3 -->
         <!-- Do the same as in Exercises 1 & 2, now with Computed Properties -->
 

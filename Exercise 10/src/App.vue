ac1d071b05ed620ba9806d5e2059b8fa446cec64
Takuya Kikuchi <kikuchi.takuya.dbce8@gmail.com>
2020-03-10 15:23:38 +0900

    exercise 10 is done

---
+++ Exercise 10/src/App.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="container">
+    <div class="row">
+      <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
+        <h1>Directives Exercise</h1>
+        <button class="btn btn-primary" v-customOn:click="clicked">Click me</button>
+        <hr />
+        <div 
+          style="width: 100px; height: 100px; background-color: lightgreen"
+          v-customOn:mouseenter="mouseEntered"
+          v-customOn:mouseleave="mouseLeft"
+        ></div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  directives: {
+    customOn: {
+      bind(el, binding) {
+        const type = binding.arg;
+        const fn = binding.value;
+        el.addEventListener(type, fn);
+      }
+    }
+  },
+  methods: {
+    clicked() {
+      alert("The button was clicked!");
+    },
+    mouseEntered() {
+      console.log("Mouse entered!");
+    },
+    mouseLeft() {
+      console.log("Mouse left!");
+    }
+  }
+};
+</script>
+
+<style>
+</style>

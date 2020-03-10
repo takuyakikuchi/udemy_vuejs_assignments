941b1301ecd2a5ef27470fef8d5a6f81d1515a90
Takuya Kikuchi <kikuchi.takuya.dbce8@gmail.com>
2020-03-10 20:07:34 +0900

    11-3 done

--- Exercise 11/src/App.vue
+++ Exercise 11/src/App.vue
@@ -3,10 +3,13 @@
     <div class="row">
       <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
         <h1>Filters & Mixins</h1>
+        <h2>With filter</h2>
         <input type="text" v-model="userInput"/>
         <div class="reverse-input">{{userInput | reverse}} ({{userInput | countLength}})</div>
-        <!-- Exercise 3 -->
-        <!-- Do the same as in Exercises 1 & 2, now with Computed Properties -->
+        <hr />
+        <h2>With computed</h2>
+        <input type="text" v-model="userInput"/>
+        <div class="reverse-input">{{ reversedInput }} ({{ countedLength }})</div>
 
         <!-- Exercise 4 -->
         <!-- Share the Computed Property rebuilding Exercise 2 via a Mixin -->
@@ -25,6 +28,14 @@ export default {
   filters: {
     reverse(value) {
       return value.split("").reverse().join("");
+    }
+  },
+  computed: {
+    reversedInput() {
+      return this.userInput.split("").reverse().join("");
+    },
+    countedLength() {
+      return this.userInput.length;
     }
   }
 };

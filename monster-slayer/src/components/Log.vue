51973ec010acb6a808e78d81ea106042a106f384
Takuya Kikuchi <kikuchi.takuya.dbce8@gmail.com>
2020-03-14 13:16:00 +0900

    added transition to log

--- monster-slayer/src/components/Log.vue
+++ monster-slayer/src/components/Log.vue
@@ -1,15 +1,19 @@
 <template>
-  <section class="row log">
-    <div class="small-12 columns">
-      <ul>
-        <li
-          v-for="log in logs"
-          :class="{'player-turn': log.isPlayer, 'monster-turn': !log.isPlayer}"
-          :key="log"
-        >{{ log.message }}</li>
-      </ul>
-    </div>
-  </section>
+  <transition name="fade">
+    <section class="row log">
+      <div class="small-12 columns">
+        <ul>
+          <transition-group name="fade">
+            <li
+              v-for="log in logs"
+              :class="{'player-turn': log.isPlayer, 'monster-turn': !log.isPlayer}"
+              :key="log"
+            >{{ log.message }}</li>
+          </transition-group>
+        </ul>
+      </div>
+    </section>
+  </transition>
 </template>
 
 <script>
@@ -35,5 +39,11 @@ export default {
 .log ul .monster-turn {
   color: red;
   background-color: #ffc0c1;
+}
+.fade-enter {
+  opacity: 0;
+}
+.fade-enter-active {
+  transition: opacity 500ms;
 }
 </style>
